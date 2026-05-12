b6ca6050f53b933b1b2e3907ff96b099f288c8aa
Adarsh uk <adarshuk79@gmail.com>
2026-05-12 10:37:29 +0530

    changes

--- server/services/newsServices.js
+++ server/services/newsServices.js
@@ -57,8 +57,45 @@ const DEFAULT_HEADERS = {
 
 async function loadPage(url) {
   const { data } = await axios.get(url, {
-    headers: DEFAULT_HEADERS,
-    timeout: 15000,
+    timeout: 20000,
+
+    headers: {
+      "User-Agent":
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/136.0.0.0 Safari/537.36",
+
+      Accept:
+        "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
+
+      "Accept-Language":
+        "en-US,en;q=0.9",
+
+      "Accept-Encoding":
+        "gzip, deflate, br",
+
+      Connection: "keep-alive",
+
+      Referer:
+        "https://www.google.com/",
+
+      DNT: "1",
+
+      "Upgrade-Insecure-Requests": "1",
+
+      "Sec-Fetch-Dest": "document",
+
+      "Sec-Fetch-Mode": "navigate",
+
+      "Sec-Fetch-Site": "none",
+
+      "Sec-Fetch-User": "?1",
+
+      Priority:
+        "u=0, i",
+
+      Pragma: "no-cache",
+
+      "Cache-Control": "no-cache",
+    },
   });
 
   return cheerio.load(data);
