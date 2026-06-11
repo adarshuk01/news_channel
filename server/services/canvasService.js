f58a6eb1ef95d8e6aadbce6c3916f02bbb851532
Adarsh uk <adarshuk79@gmail.com>
2026-06-11 16:26:51 +0530

    changes

--- server/services/canvasService.js
+++ server/services/canvasService.js
@@ -3,7 +3,7 @@ const path  = require("path");
 const sharp = require("sharp");
 
 GlobalFonts.registerFromPath(
-  path.join(__dirname, "../fonts/NotoSansMalayalam-Bold.ttf"),
+  path.join(__dirname, "../fonts/AnekMalayalam-Bold.ttf"),
   "Malayalam"
 );
 GlobalFonts.registerFromPath(
